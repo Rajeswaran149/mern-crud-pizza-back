709cb1845374ce1e529d1545d8bb4a4d60423eae
Rajeswaran <rajeswaran149@gmail.com>
2024-05-27 12:23:22 +0530

    update

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ app.use(
 
 app.get('/', (req, res) => {
   res.send('Web server started running');
+  res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+
 });
 
 const pizzasRoute = require('./routes/pizzasRoute');
