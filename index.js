66e04b36bb4cd89aed8e7ef149c8a4250763dc35
Rajeswaran <rajeswaran149@gmail.com>
2024-05-29 12:38:00 +0530

    update

--- index.js
+++ index.js
@@ -24,11 +24,6 @@ app.use(
 
 app.get('/', (req, res) => {
   res.send('Web server started running');
-//   res.setHeader("Access-Control-Allow-Origin", "*")
-// res.setHeader("Access-Control-Allow-Credentials", "true");
-// res.setHeader("Access-Control-Max-Age", "1800");
-// res.setHeader("Access-Control-Allow-Headers", "content-type");
-// res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
 
 });
 
